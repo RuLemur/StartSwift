b05dad1d20069fbf5f747c7426ce8a4e676c244b
rulemur <rulemur@gmail.com>
2020-01-22 20:39:00 +0300

    change file

---
+++ lesson_2/lesson_2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  lesson_2
+//
+//  Created by Александр Павлов on 22.01.2020.
+//  Copyright © 2020 Александр Павлов. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
